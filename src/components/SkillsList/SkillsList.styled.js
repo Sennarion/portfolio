168efcf740bf44485737f0bb2d225e9a5ef87bc2
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-04-01 18:23:10 +0300

    add projects styles

--- src/components/SkillsList/SkillsList.styled.js
+++ src/components/SkillsList/SkillsList.styled.js
@@ -14,20 +14,39 @@ export const List = styled.ul`
 `;
 
 export const SkillName = styled.h3`
+  position: relative;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
   gap: ${({ theme }) => theme.spacing(4)};
-  margin-bottom: ${({ theme }) => theme.spacing(6)};
-  transition: color ${({ theme }) => theme.animation.cubicBezier};
+  text-align: center;
+  color: ${({ theme }) => theme.colors.grey};
+  width: 30%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.white};
+  transition: background-color ${({ theme }) => theme.animation.cubicBezier};
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    margin-top: -10px;
+    right: -20px;
+    border: solid 10px transparent;
+    border-left-color: ${({ theme }) => theme.colors.white};
+    z-index: 1;
+    transition: border-color ${({ theme }) => theme.animation.cubicBezier};
+  }
 `;
 
 export const ListItem = styled.li`
   flex-basis: 100%;
-
+  min-height: 240px;
+  display: flex;
   background: ${({ theme }) => theme.colors.grey};
   border: 2px solid transparent;
-  border-radius: ${({ theme }) => theme.spacing(4)} ${({ theme }) => theme.spacing(2)};
-  padding: ${({ theme }) => theme.spacing(5)};
+  /* border-radius: ${({ theme }) => theme.spacing(4)} ${({ theme }) => theme.spacing(2)}; */
   transition: box-shadow ${({ theme }) => theme.animation.cubicBezier},
     border ${({ theme }) => theme.animation.cubicBezier};
 
@@ -36,16 +55,26 @@ export const ListItem = styled.li`
     border: 2px solid ${({ theme }) => theme.colors.primary};
 
     ${SkillName} {
-      color: ${({ theme }) => theme.colors.primary};
+      background-color: ${({ theme }) => theme.colors.primary};
+
+      &::after {
+        border-left-color: ${({ theme }) => theme.colors.primary};
+      }
     }
   }
 
-  @media screen and (min-width: ${({ theme }) => theme.media.tablet}) {
+  @media screen and (min-width: ${({ theme }) => theme.media.desktop}) {
     flex-basis: calc((100% - 20px) / 2);
   }
 `;
 
 export const AbilitiesList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 70%;
+  padding: ${({ theme }) => theme.spacing(6)};
+  padding-left: ${({ theme }) => theme.spacing(10)};
   list-style: inside;
 `;
 
--- src/data/skills.js
+++ src/data/skills.js
@@ -14,20 +14,27 @@ export const skills = [
   {
     title: 'HTML5',
     icon: AiFillHtml5,
-    abilities: ['tags and attributes', 'semantic structure', 'validation']
+    abilities: [
+      'tags and attributes',
+      'semantic structure',
+      'accessibility and validation',
+      'text and images',
+      'forms and tables',
+      'links and buttons'
+    ]
   },
   {
     title: 'CSS3',
     icon: DiCss3,
     abilities: [
-      'box model',
-      'layout techniques',
-      'preprocessors',
+      'box model and positioning',
+      'layout techniques (flexbox, grid)',
+      'pseudo-elements and pseudo-classes',
+      'preprocessors (SASS, SCSS)',
       'cross-browser compatibility',
       'responsive/adaptive layout',
-      'transitions/animations',
-      'positioning',
-      'BEM'
+      'transitions and animations',
+      'BEM methodology'
     ]
   },
   {
@@ -35,14 +42,12 @@ export const skills = [
     icon: SiJavascript,
     abilities: [
       'variables and types',
-      'operators',
-      'branches and cycles',
       'data structures',
-      'functions',
+      'operators and functions',
+      'branches and loops',
+      'DOM, events and web storage',
       'prototypes and classes',
-      'DOM, events',
-      'web storage',
-      'asynchronous JS'
+      'event loop and asynchronous JS'
     ]
   },
   {
@@ -55,46 +60,62 @@ export const skills = [
     icon: DiReact,
     abilities: [
       'class/function components',
-      'collections',
-      'stylization',
-      'events and state',
-      'forms',
-      'lifecycle',
-      'hooks',
-      'routing',
-      'HTTP requests'
+      'state and events',
+      'lifecycle and hooks',
+      'routing and code splitting',
+      'authorization and HTTP requests',
+      'hocs and custom hooks'
     ]
   },
   {
     title: 'Redux',
     icon: SiRedux,
     abilities: [
-      'Redux DevTools',
-      'Redux Toolkit',
-      'Redux Thunk',
-      'actions',
-      'reducers',
-      'selectors'
+      'toolkit, store and slices',
+      'actions, reducers, selectors',
+      'asynchronous operations (thunk)',
+      'devTools and debugging'
     ]
   },
   {
     title: 'Styled Components',
     icon: SiStyledcomponents,
-    abilities: ['global styles', 'theming', 'props drilling', 'reusable components']
+    abilities: [
+      'global styles',
+      'props and dynamic styles',
+      'theming and keyframes',
+      'nested selectors',
+      'reusable components'
+    ]
   },
   {
     title: 'Material UI',
     icon: SiMui,
-    abilities: ['components', 'theming', 'icons']
+    abilities: [
+      'layout, data display and surfaces',
+      'inputs and feedback',
+      'navigation and utils',
+      'components API and customization'
+    ]
   },
   {
     title: 'GIT',
     icon: BsGit,
-    abilities: ['terminal skills', 'repositories', 'branches', 'pull requests']
+    abilities: [
+      'terminal skills',
+      'manage repositories',
+      'branching and merging',
+      'committing and pushing',
+      'feature branches and pull requests'
+    ]
   },
   {
     title: 'Figma',
     icon: SiFigma,
-    abilities: ['components and styles', 'layout', 'typography']
+    abilities: [
+      'components and graphic elements',
+      'layer organization and styles',
+      'formats and resolutions'
+    ]
   }
 ];
